ac56dd34758d6b36372f4f165b35dd85c37c93e4
alefaso-lucky <ale.fasolino02@gmail.com>
2025-05-16 12:51:11 +0200

    Realizzata la classe astratta Forma

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,10 @@ module it.unisa.software_architecture_design.drawsnapdrawingtool {
 
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
+    requires java.desktop;
 
     opens it.unisa.software_architecture_design.drawsnapdrawingtool to javafx.fxml;
     exports it.unisa.software_architecture_design.drawsnapdrawingtool;
+    exports it.unisa.software_architecture_design.drawsnapdrawingtool.forma;
+    opens it.unisa.software_architecture_design.drawsnapdrawingtool.forma to javafx.fxml;
 }
