227d116f8b623fe737af10990431cdde095de094
francescopiocirillo <francescopiocirillo.c@gmail.com>
2025-05-19 15:14:29 +0200

    Creazione classe SaveCommandTest

---
+++ src/test/java/it/unisa/software_architecture_design/drawsnapdrawingtool/commands/SaveCommandTest.java
@@ -0,0 +1,12 @@
+package it.unisa.software_architecture_design.drawsnapdrawingtool.commands;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SaveCommandTest {
+
+    @Test
+    void execute() {
+    }
+}
