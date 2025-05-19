e4244afa217f1aac0fd741918ef5d5b21fc02af0
RaffaeleCalabrese2 <raffaelecalabrese2802@gmail.com>
2025-05-19 17:45:26 +0200

    aggiunti test per la modale di inserimento

--- src/test/java/it/unisa/software_architecture_design/drawsnapdrawingtool/interactionstate/DrawStateTestUI.java
+++ src/test/java/it/unisa/software_architecture_design/drawsnapdrawingtool/interactionstate/DrawStateTestUI.java
@@ -6,6 +6,7 @@ import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,10 +14,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Classe di test automatica per la classe {@link DrawState}, focalizzata
@@ -57,12 +61,12 @@ public class DrawStateTestUI extends ApplicationTest {
     /**
      * Testa il metodo {@code helpUIHandleMousePressed} della classe {@link DrawState}
      * verificando che, in seguito alla conferma della finestra di dialogo per la selezione
-     * degli attributi di una forma, vengano restituiti correttamente gli attributi scelti.
+     * degli attributi di un rettangolo, vengano restituiti correttamente gli attributi scelti.
      * @throws Exception se si verifica un'interruzione durante l'attesa del completamento
      *                   del thread JavaFX o altre eccezioni durante il test.
      */
     @Test
-    void testHelpUIHandleMousePressed_Dialog() throws Exception {
+    void testHelpUIHandleMousePressed_Rettangolo() throws Exception {
         CountDownLatch latch = new CountDownLatch(1);
         final AttributiForma[] result = new AttributiForma[1];
 
@@ -90,12 +94,70 @@ public class DrawStateTestUI extends ApplicationTest {
         assertEquals(0.0, attributi.getAngoloInclinazione(), 0.01, "Valore di angolo errato");
     }
 
+    /**
+     * Testa il metodo {@code helpUIHandleMousePressed} della classe {@link DrawState}
+     * verificando che, in seguito alla conferma della finestra di dialogo per la selezione
+     * degli attributi di una linea, vengano restituiti correttamente gli attributi scelti.
+     * @throws Exception se si verifica un'interruzione durante l'attesa del completamento
+     *                   del thread JavaFX o altre eccezioni durante il test.
+     */
+    @Test
+    void testHelpUIHandleMousePressed_Linea() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        final AttributiForma[] result = new AttributiForma[1];
+
+        Platform.runLater(() -> {
+            try {
+                DrawState state = new DrawState(Forme.LINEA);
+                result[0] = state.helpUIHandleMousePressed(Forme.LINEA);
+            } finally {
+                latch.countDown();
+            }
+        });
+
+        if (!latch.await(10, TimeUnit.SECONDS)) {
+            fail("Timeout scaduto per l'interazione con JavaFX");
+        }
+
+        AttributiForma attributi = result[0];
+
+        assertNotNull(attributi, "La finestra di dialogo non ha restituito attributi");
+        assertNotNull(attributi.getColore(), "Il colore del bordo non deve essere nullo");
+        assertEquals(Color.TRANSPARENT, attributi.getColoreInterno(), "Il colore interno di una linea deve essere trasparente");
+        assertEquals(0.0, attributi.getAltezza(), 0.01, "L'altezza di una linea deve essere 0");
+
+        assertEquals(100.0, attributi.getLarghezza(), 0.01, "Larghezza non corretta");
+        assertEquals(0.0, attributi.getAngoloInclinazione(), 0.01, "Angolo di inclinazione non corretto");
+    }
 
 
+    /**
+     * Testa il metodo {@code helpUIHandleMousePressed} della classe {@link DrawState}
+     * quando l'utente preme il pulsante "Annulla" nella finestra di dialogo.
+     * @throws Exception se si verifica un'interruzione durante l'attesa del completamento
+     * del thread JavaFX o altre eccezioni durante il test.
+     */
+    @Test
+    void testHelpUIHandleMousePressed_Annulla_Dialog() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        final AttributiForma[] result = new AttributiForma[1];
 
+        Platform.runLater(() -> {
+            try {
+                DrawState state = new DrawState(Forme.RETTANGOLO);
+                result[0] = state.helpUIHandleMousePressed(Forme.RETTANGOLO);
+            } finally {
+                latch.countDown();
+            }
+        });
 
+        if (!latch.await(10, TimeUnit.SECONDS)) {
+            fail("Timeout scaduto per l'interazione con JavaFX");
+        }
 
-
+        AttributiForma attributi = result[0];
+        assertNull(attributi, "Se si preme Annulla, il metodo deve restituire null");
+    }
 
 //    @Test
 //    void getFormaCorrente() {
