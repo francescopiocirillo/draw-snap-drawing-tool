057154abf65c46577429908be48f615f5669e58f
alefaso-lucky <ale.fasolino02@gmail.com>
2025-06-02 10:23:27 +0200

    modifiche UI icone

--- src/main/java/it/unisa/software_architecture_design/drawsnapdrawingtool/DrawSnapController.java
+++ src/main/java/it/unisa/software_architecture_design/drawsnapdrawingtool/DrawSnapController.java
@@ -267,8 +267,8 @@ public class DrawSnapController {
         imageCopy = new ImageView(new Image(getClass().getResourceAsStream("/it/unisa/software_architecture_design/drawsnapdrawingtool/images/copia.png")));
         imageCut = new ImageView(new Image(getClass().getResourceAsStream("/it/unisa/software_architecture_design/drawsnapdrawingtool/images/taglia.png")));
         imagePaste = new ImageView(new Image(getClass().getResourceAsStream("/it/unisa/software_architecture_design/drawsnapdrawingtool/images/incolla.png")));
-        imageCompose = new ImageView(new Image(getClass().getResourceAsStream("/it/unisa/software_architecture_design/drawsnapdrawingtool/images/incolla.png")));
-        imageDecompose = new ImageView(new Image(getClass().getResourceAsStream("/it/unisa/software_architecture_design/drawsnapdrawingtool/images/incolla.png")));
+        imageCompose = new ImageView(new Image(getClass().getResourceAsStream("/it/unisa/software_architecture_design/drawsnapdrawingtool/images/raggruppa.png")));
+        imageDecompose = new ImageView(new Image(getClass().getResourceAsStream("/it/unisa/software_architecture_design/drawsnapdrawingtool/images/annullaRaggruppa.png")));
         imageCopy.setFitWidth(16);
         imageCut.setFitWidth(16);
         imagePaste.setFitWidth(16);
